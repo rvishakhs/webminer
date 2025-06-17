c933eae25d11651c75202013d11af72110857cad
rvishakhs <rvishakhs@gmail.com>
2025-06-17 22:49:10 +0100

    signin page working

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)",
-    "/sign-up(.*)","/*"
-])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
 })
-
 
 export const config = {
   matcher: [
@@ -19,4 +15,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
