d6489f57dc691dc91363d94223685717fe49d749
rvishakhs <rvishakhs@gmail.com>
2025-07-02 09:34:08 +0100

    ok error striked need fix

--- src/lib/workflow/executor/LaunchBrowserExecutor.ts
+++ src/lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,6 +1,17 @@
-
+import puppeteer from 'puppeteer';
+import { waitFor } from '~/lib/helper/waitFor';
 
 export async function LaunchBrowserExecutor(): Promise<boolean> {
-    console.log("running launch browser executor")
-    return true
+    try {
+        const browser = await puppeteer.launch({
+            headless: false // for testing 
+        })
+
+        await waitFor(3000)
+        await browser.close()
+        return true;
+    } catch (error) {
+        console.error("Error in LaunchBrowserExecutor:", error)
+        return false
+    }
 }
