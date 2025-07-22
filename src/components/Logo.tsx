efd6148758c31001491b5992414175172aee4370
rvishakhs <rvishakhs@gmail.com>
2025-07-22 23:32:22 +0100

    only two jobs are pending create a rep[ort page and show user name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import React from 'react'
 import { cn } from '~/lib/utils';
 import { GiSpiderWeb } from "react-icons/gi";
+import { MdScreenSearchDesktop } from "react-icons/md";
 
 
 function Logo({
@@ -16,11 +17,11 @@ function Logo({
     <div>
         <Link href="/" className={cn("text-2xl font-extrabold flex items-center gap-2", fontSize)}>
             <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600 p-2">
-                <SquareDashedMousePointer size={iconSize} className='stroke-white' />
+                <MdScreenSearchDesktop size={iconSize} className='stroke-white' />
                 {/* <GiSpiderWeb size={iconSize} className='stroke-white' /> */}
             </div>
             <div>
-                <span className="bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
+                <span className="bg-gradient-to-r font from-emerald-500 to-emerald-600 bg-clip-text text-transparent">
                     Web
                 </span>
                 <span className="text-stone-700 dark:text-stone-300">Miner</span>
